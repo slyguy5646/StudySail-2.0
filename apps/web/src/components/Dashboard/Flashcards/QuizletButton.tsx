6a799a30f81bb9c2ee16fe579ad5ca08c59b4ee0
slyguy5646 <lpmonaghan06@yahoo.com>
2023-07-29 19:58:41 -0400

    fixed quizlet button image

--- apps/web/src/components/Dashboard/Flashcards/QuizletButton.tsx
+++ apps/web/src/components/Dashboard/Flashcards/QuizletButton.tsx
@@ -5,6 +5,7 @@ import { Flashcard } from "@prisma/client";
 import { useState, useEffect } from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { useToast } from "@/components/ui/use-toast";
+import quizlet from "../../../../public/quizlet.png";
 
 export default function QuizletButton({ cards }: { cards: Flashcard[] }) {
   const [formatted, setFormatted] = useState<string | null>(null);
@@ -15,7 +16,7 @@ export default function QuizletButton({ cards }: { cards: Flashcard[] }) {
   }, [cards]);
 
   return (
-    <TooltipProvider >
+    <TooltipProvider>
       <Tooltip>
         <TooltipTrigger>
           <div
@@ -24,13 +25,14 @@ export default function QuizletButton({ cards }: { cards: Flashcard[] }) {
                 navigator.clipboard.writeText(formatted);
                 toast({
                   title: "Copied Quizlet Format!",
-                  description: "You just copied a quizlet compatible format from these flashcards. Read the docs for more info.",
+                  description:
+                    "You just copied a quizlet compatible format from these flashcards. Read the docs for more info.",
                 });
               }
             }}
           >
             <Image
-              src={"https://assets.quizlet.com/a/j/dist/app/i/brandmark/1024.0e9431247202b7b.png"}
+              src={quizlet}
               width={40}
               height={40}
               className="hover:cursor-pointer rounded-md select-none"
